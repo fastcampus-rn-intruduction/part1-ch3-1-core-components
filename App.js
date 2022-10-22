0793b56cf09b9c83de917964ea2aaa77cf937de6
daheeahn <deg9810@gmail.com>
2022-10-22 13:23:34 +0900

    최종 코드

--- App.js
+++ App.js
@@ -1,11 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Image source={require("./cute-puppy.jpeg")} style={styles.local_image} />
+      <Image
+        source={{
+          uri: "http://img.khan.co.kr/news/2019/11/29/l_2019112901003607500286631.jpg",
+        }}
+        style={styles.url_image}
+      />
+
+      <TextInput placeholder="이름을 입력해주세요" />
+
+      <Button
+        title="Click Me!"
+        onPress={() => {
+          console.log("clicked!");
+        }}
+      />
+
+      <Switch value={true} />
+      <Switch value={false} />
+
+      <ScrollView>
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+        <Image
+          source={require("./cute-puppy.jpeg")}
+          style={styles.local_image}
+        />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +86,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  local_image: {
+    width: 100,
+    height: 100,
+  },
+  url_image: {
+    width: 200,
+    height: 200,
   },
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+### 패스트캠퍼스 React Native 앱 개발
+
+> Part 1
+
+> Chapter 3 - 1 - Core Components 코드입니다.
